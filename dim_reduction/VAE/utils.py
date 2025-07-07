44574ea03d01c90551bd969e8443ea6a68921e32
AIAI333 <12312107@mail.sustech.edu.cn>
2025-07-07 15:17:02 +0800

    Update

--- dim_reduction/VAE/utils.py
+++ dim_reduction/VAE/utils.py
@@ -1,3 +1,4 @@
+from .ae import AutoEncoder
 from .vae import VariationalAutoEncoder
 from .sparse_vae import SparseVAE
 def get_vae(args, input_dim):
@@ -5,5 +6,7 @@ def get_vae(args, input_dim):
         return VariationalAutoEncoder(input_dim=input_dim, latent_dim=args.latent_dim if args.latent_dim else 10)
     elif args.vae == "svae":
         return SparseVAE(input_dim=input_dim, latent_dim=args.latent_dim if args.latent_dim else 10)
+    elif args.vae == "ae":
+        return AutoEncoder(input_dim=input_dim, latent_dim=args.latent_dim if args.latent_dim else 10)
     else:
         raise NotImplementedError
